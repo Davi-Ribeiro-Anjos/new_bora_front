d2382901cac8ca9974ef4f64d0384de45846d90a
Davi <davi.ribeirodosanjos@gmail.com>
2023-06-13 15:55:13 -0300

    feat: criando cads para info

--- src/components/paletes/infoPalete.jsx
+++ src/components/paletes/infoPalete.jsx
@@ -1,4 +1,21 @@
+import { Col, Panel, Placeholder, Row } from 'rsuite';
 import MainModal from '../modal';
+
+const styles = {
+    col: {
+        margin: "10px 0",
+    },
+}
+
+const Card = ({ titulo }) => (
+    <Col md={4} sm={12} style={styles.col}>
+        <Panel bordered header={titulo}>
+            <p>PBR: <strong>1</strong></p>
+            <p>CHEP: <strong>1</strong></p>
+            <p>TOTAL: <strong>1</strong></p>
+        </Panel>
+    </Col>
+);
 
 const InfoPalete = ({ abrirInfo, setAbrirInfo }) => {
     const fechar = () => {
@@ -6,8 +23,15 @@ const InfoPalete = ({ abrirInfo, setAbrirInfo }) => {
     };
 
     return (
-        <MainModal open={abrirInfo} setOpen={setAbrirInfo} titulo="Quantidade de Paletes por Garagem" nomeBotao="Info" view={true} size='full' fechar={fechar} >
-
+        <MainModal open={abrirInfo} setOpen={setAbrirInfo} titulo="Quantidade de Paletes por Garagem" nomeBotao="Adicionar Paletes" size='full' fechar={fechar} >
+            <Row>
+                <Card titulo={"teste"} />
+                <Card titulo={"teste"} />
+                <Card titulo={"teste"} />
+                <Card titulo={"teste"} />
+                <Card titulo={"teste"} />
+                <Card titulo={"teste"} />
+            </Row>
         </MainModal>
     )
 }
